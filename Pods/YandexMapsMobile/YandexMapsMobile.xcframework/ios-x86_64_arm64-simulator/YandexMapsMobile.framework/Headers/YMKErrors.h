f8b43917fe0eebc791e16a5a76ef295801ccd85a
MargaritaUsova <askacziaa@gmail.com>
2024-02-11 23:40:16 +0300

    initial commit

--- Pods/YandexMapsMobile/YandexMapsMobile.xcframework/ios-x86_64_arm64-simulator/YandexMapsMobile.framework/Headers/YMKErrors.h
+++
@@ -1,9 +0,0 @@
-#import <YandexMapsMobile/YRTExport.h>
-#import <YandexMapsMobile/YRTLocalError.h>
-
-/**
- * Cache path doesn't exist or there no read permissions.
- */
-YRT_EXPORT @interface YMKCachePathUnavailable : YRTLocalError
-
-@end
