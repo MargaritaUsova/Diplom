50a9c2824f5bd9cba17b868cad0681db883b1ea8
MargaritaUsova <askacziaa@gmail.com>
2024-02-21 21:43:00 +0300

    test commit

--- Diplom/AppDelegate.swift
+++ Diplom/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import YandexMapsMobile
+import CoreLocation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
---
+++ Diplom/Model/SearchModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchModel.swift
+//  Diplom
+//
+//  Created by Margarita Usova on 13.02.2024.
+//
+
+import Foundation
