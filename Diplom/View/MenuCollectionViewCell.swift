e5b1e7823ba96c6c2d57f2f122f084867b7658ba
MargaritaUsova <askacziaa@gmail.com>
2024-04-16 11:55:46 +0300

    исправлен SearchViewController

---
+++ Diplom/View/MenuCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MenuCollectionViewCell.swift
+//  Diplom
+//
+//  Created by Margarita Usova on 15.04.2024.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    
+}
